ce8b19fd933fadb055a673c006d87e3ea94b64cb
Tasnim <tasnim.mostari@gmail.com>
2021-10-31 19:04:59 +0600

    myTrip added

--- src/Pages/MyTrips/MyTrips/MyTrips.js
+++ src/Pages/MyTrips/MyTrips/MyTrips.js
@@ -1,21 +1,84 @@
 import React, { useEffect, useState } from 'react';
-import { Row } from 'react-bootstrap';
+import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
 
 const MyTrips = () => {
 
-    const [myTrips, setMyTrips] = useState({})
+    const [myTrips, setMyTrips] = useState([])
 
-    useEffect('')
+    const { user } = useAuth();
+
+    // var trip = '';
+
+    useEffect(() => {
+
+        const url = `http://localhost:5000/bookings`;
+        fetch(url)
+            .then(res => res.json())
+            .then(datas => {
+
+                const trip = datas.filter(data => data.email == user.email)
+                setMyTrips(trip);
+
+            })
+
+    }, [])
+
+
+    const handleDeleteUser = id => {
+        const proceed = window.confirm('Delete sure?')
+        if (proceed) {
+            const url = `http://localhost:5000/bookings/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully')
+                        const restOrders = myTrips.filter(order => order._id !== id)
+                        setMyTrips(restOrders);
+                    }
+                })
+        }
+
+    }
+
 
 
     return (
         <div>
             <h2>My Trips</h2>
-            {/* <Row xs={1} md={2} className="g-4">
-            {
-                // myTrips.map(myTrip => console.log(myTrip))
-            }
-        </Row> */}
+            <Row xs={1} md={2} className="g-4">
+                {
+                    myTrips.map(myTrip => <Col key={myTrip._id}>
+                        <Card className='card-style'>
+                            <Card.Body>
+                                <Card.Title>Name: {myTrip?.name}</Card.Title>
+                                <Card.Text>
+                                    Email: {myTrip?.email}
+                                </Card.Text>
+                                <Card.Text>
+                                    Booking id: {myTrip?.booking}
+                                </Card.Text>
+                                <Card.Text>
+                                    Date: {myTrip?.date}
+                                </Card.Text>
+                                <Card.Text>
+                                    Phone: {myTrip?.phone}
+                                </Card.Text>
+                                <Card.Text>
+                                    Status: {myTrip?.status}
+                                </Card.Text>
+
+                                <Button onClick={() => handleDeleteUser(myTrip._id)} className='book-btn'>Delete</Button>
+
+                            </Card.Body>
+                        </Card>
+                    </Col>)
+                }
+            </Row>
 
         </div>
     );
